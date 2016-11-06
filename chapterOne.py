f1dce2256cac1fcdc7dc3ced1e0c62867119b08a
JonathanGrant <jonathanallengrant@gmail.com>
2016-11-06 02:22:00 -0800

    Added sorted two sum

--- chapterOne.py
+++ chapterOne.py
@@ -8,5 +8,20 @@ def twoSum(numArray, targetNum):
 				return indexOne, indexTwo
 	return -1, -1
 
+def sortedTwoSum(numArray, targetNum):
+	if len(numArray) < 2:
+		return -1, -1
+	indexOne = 0
+	indexTwo = 1
+	while indexOne < indexTwo:
+		if numArray[indexOne] + numArray[indexTwo] == targetNum:
+			return indexOne, indexTwo
+		elif numArray[indexOne] + numArray[indexTwo] > targetNum:
+			indexTwo -= 1
+		else:
+			indexOne += 1
+			indexTwo = len(numArray) - 1
+	return -1, -1
+
 myArr = [0, 1, 2, 3, 4 ,5 ,6, 7, 8, 9, 10]
-print twoSum(myArr, 13)
+print sortedTwoSum(myArr, 13)
