ced78e3b3d1d12651fe85877ebb6ab71d3332d56
JonathanGrant <jonathanallengrant@gmail.com>
2016-11-09 11:37:49 -0800

    Linked List has cycle plus implementation of linked list

---
+++ linkedListCycle.py
@@ -0,0 +1,32 @@
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+	def setNext(self, next):
+		self.next = next
+	def getNext(self):
+		return self.next
+
+
+
+def hasCycle(node):
+	if node:
+		hashMap = {}
+		while node.getNext():
+			if id(node) in hashMap:
+				return True
+			hashMap[id(node)] = 1
+			node = node.getNext()
+	return False
+
+#Create a bunch of nodes for my linkedlist
+firstNode = Node(0)
+prevNode = firstNode
+for i in range(1, 10):
+	newNode = Node(i)
+	prevNode.setNext(newNode)
+	prevNode = newNode
+print hasCycle(None)
+print hasCycle(firstNode)
+prevNode.setNext(firstNode)
+print hasCycle(firstNode)
